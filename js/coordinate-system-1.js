0fdd108ac7e706d47e230f7d6cc408c7055b2abf
Tom Hardcastle <tomhardcastle@hotmail.co.uk>
2021-12-06 01:57:18 +0000

    Style changes

--- js/coordinate-system-1.js
+++ js/coordinate-system-1.js
@@ -38,16 +38,24 @@ function makeDraggable(evt) {
             if (selectedElement.id == "pointA"){
                 document.getElementById("lineSection").setAttribute("x1", newCircleXPosition);
                 document.getElementById("lineSection").setAttribute("y1", newCircleYPosition);
+
+                document.getElementById("pointALabel").setAttribute("x", newCircleXPosition);
+                document.getElementById("pointALabel").setAttribute("y", newCircleYPosition - 16);
+
                 document.getElementById("pointAXValue").innerHTML = convertX(newCircleXPosition).toPrecision(4);
                 document.getElementById("pointAYValue").innerHTML = convertY(newCircleYPosition).toPrecision(4);
             }
             else {
                 document.getElementById("lineSection").setAttribute("x2", newCircleXPosition);
                 document.getElementById("lineSection").setAttribute("y2", newCircleYPosition);
+
+                document.getElementById("pointBLabel").setAttribute("x", newCircleXPosition);
+                document.getElementById("pointBLabel").setAttribute("y", newCircleYPosition - 16);
+
                 document.getElementById("pointBXValue").innerHTML = convertX(newCircleXPosition).toPrecision(4);
                 document.getElementById("pointBYValue").innerHTML = convertY(newCircleYPosition).toPrecision(4);
             }
-            updateCentreOfMass(
+            updateCentroid(
                 document.getElementById("pointA").getAttributeNS(null, "cx"),
                 document.getElementById("pointB").getAttributeNS(null, "cx"),
                 document.getElementById("pointA").getAttributeNS(null, "cy"),
@@ -60,17 +68,17 @@ function makeDraggable(evt) {
     }
 }
 
-function updateCentreOfMass(pointAXValue, pointBXValue, pointAYValue, pointBYValue){
-    let centreOfMassX = (parseFloat(pointBXValue) + parseFloat(pointAXValue)) / 2;
-    let centreOfMassY = (parseFloat(pointBYValue) + parseFloat(pointAYValue)) / 2;
+function updateCentroid(pointAXValue, pointBXValue, pointAYValue, pointBYValue){
+    let centroidX = (parseFloat(pointBXValue) + parseFloat(pointAXValue)) / 2;
+    let centroidY = (parseFloat(pointBYValue) + parseFloat(pointAYValue)) / 2;
     let distanceX = convertX(pointBXValue) - convertX(pointAXValue);
     let distanceY = convertY(pointBYValue) - convertY(pointAYValue);
     let vectorMagnitude = Math.sqrt(Math.pow(distanceX, 2)+Math.pow(distanceY,2));
 
-    document.getElementById("centreOfMass").setAttribute("cx", centreOfMassX);
-    document.getElementById("centreOfMass").setAttribute("cy", centreOfMassY);
-    document.getElementById("comXValue").innerHTML = convertX(centreOfMassX).toPrecision(4);
-    document.getElementById("comYValue").innerHTML = convertY(centreOfMassY).toPrecision(4);
+    document.getElementById("centroid").setAttribute("cx", centroidX);
+    document.getElementById("centroid").setAttribute("cy", centroidY);
+    document.getElementById("centroidXValue").innerHTML = convertX(centroidX).toPrecision(4);
+    document.getElementById("centroidYValue").innerHTML = convertY(centroidY).toPrecision(4);
     document.getElementById("separationAB").innerHTML = vectorMagnitude.toPrecision(4);
 
     let opposite = distanceY;
@@ -91,11 +99,15 @@ function updateCentreOfMass(pointAXValue, pointBXValue, pointAYValue, pointBYVal
 function resetPoints(){
     document.getElementById("pointA").setAttribute("cx", 54);
     document.getElementById("pointA").setAttribute("cy", 446);
+    document.getElementById("pointALabel").setAttribute("x", 48);
+    document.getElementById("pointALabel").setAttribute("y", 430);
     document.getElementById("pointAXValue").innerHTML = "10.00";
     document.getElementById("pointAYValue").innerHTML = "10.00";
 
     document.getElementById("pointB").setAttribute("cx", 250);
     document.getElementById("pointB").setAttribute("cy", 250);
+    document.getElementById("pointBLabel").setAttribute("x", 244);
+    document.getElementById("pointBLabel").setAttribute("y", 234);
     document.getElementById("pointBXValue").innerHTML = "50.00";
     document.getElementById("pointBYValue").innerHTML = "50.00";
 
@@ -104,10 +116,10 @@ function resetPoints(){
     document.getElementById("lineSection").setAttribute("x2", 250);
     document.getElementById("lineSection").setAttribute("y2", 250);
 
-    document.getElementById("centreOfMass").setAttribute("cx", 152);
-    document.getElementById("centreOfMass").setAttribute("cy", 348);
-    document.getElementById("comXValue").innerHTML = "30.00";
-    document.getElementById("comYValue").innerHTML = "30.00";
+    document.getElementById("centroid").setAttribute("cx", 152);
+    document.getElementById("centroid").setAttribute("cy", 348);
+    document.getElementById("centroidXValue").innerHTML = "30.00";
+    document.getElementById("centroidYValue").innerHTML = "30.00";
     document.getElementById("separationAB").innerHTML = "56.57";
     document.getElementById("angleAB").innerHTML = "45.00";
 }
